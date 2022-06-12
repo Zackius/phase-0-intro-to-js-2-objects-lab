917efa21105e90876ede162bea51d3c1adc087d1
Zacharia <ndunguzachary24@gmail.com>
2022-06-12 23:04:56 +0300

    commit tree 2b2e29e651e34afc57483929d1b2f10aac0a828b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Bob",
+    streetAddress: "Githurai"
+  
+}
+
+ const updateEmployeeWithKeyAndValue =  (employee, name, Bob) =>{
+
+    let worker = {...employee}
+  
+    worker  [name] = Bob
+
+  return worker
+}
+ const destructivelyUpdateEmployeeWithKeyAndValue = (employee, name, Sam) =>{
+employee[name] = Sam
+return employee
+ }
+  const deleteFromEmployeeByKey = (employee, name) =>{
+let wafanyakazi ={...employee}
+delete wafanyakazi[name]
+return wafanyakazi
+ }
+const  destructivelyDeleteFromEmployeeByKey=(employee, name) =>{
+ delete employee[name]
+
+ return employee 
+ }
